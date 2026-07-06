9098fe1782612a1a754d317748933340476f95a4
Bo Li <22713281+bobboli@users.noreply.github.com>
2026-07-06 08:35:01 +0000

    refactor: rename FSDP zero-SM option
    Signed-off-by: Bo Li <22713281+bobboli@users.noreply.github.com>

--- megatron/training/arguments.py
+++ megatron/training/arguments.py
@@ -1280,7 +1280,7 @@ def validate_args(args, defaults={}):
             args.ckpt_format == "fsdp_dtensor"
         ), "Megatron-FSDP requires the `fsdp_dtensor` checkpointing format."
 
-    if args.megatron_fsdp_zero_sm_all_gather:
+    if args.fsdp_zero_sm_allgather:
         assert (
             args.use_megatron_fsdp
         ), "Megatron-FSDP zero-SM all-gather requires --use-megatron-fsdp."
@@ -4025,9 +4025,9 @@ def _add_distributed_args(parser):
         'This option will force to use conventional (local) userbuffer registration when use-nccl-ub is set.',
     )
     group.add_argument(
-        '--megatron-fsdp-zero-sm-all-gather',
+        '--fsdp-zero-sm-allgather',
         action='store_true',
-        dest='megatron_fsdp_zero_sm_all_gather',
+        dest='fsdp_zero_sm_allgather',
         default=False,
         help='Request NCCL zero-CTA/copy-engine collectives for eligible Megatron-FSDP '
         'parameter all-gather buffers. Requires --use-megatron-fsdp, --use-nccl-ub, '
